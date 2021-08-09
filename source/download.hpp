dd339a7714154e0396fdfb6e2bee3dd93606738d
Tobi <tobid7@outlook.de>
2021-08-09 16:33:49 +0200

    Create download.hpp

---
+++ source/download.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <3ds.h>
+
+#define APP_TITLE "DevHelper"
+#define VERSION_STRING "1.0.0"
+
+enum DownloadError {
+	DL_ERROR_NONE = 0,
+	DL_ERROR_WRITEFILE,
+	DL_ERROR_ALLOC,
+	DL_ERROR_STATUSCODE,
+	DL_ERROR_GIT,
+	DL_CANCEL, // No clue if that's needed tho.
+};
+
+
+Result downloadToFile(const std::string &url, const std::string &path);
+//Result downloadFromRelease(const std::string &url, const std::string &asset, const std::string &path, bool includePrereleases);
