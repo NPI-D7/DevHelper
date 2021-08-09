dc421f23c0cca4b0a695dfca1e4e12dd0515ec91
Tobi <tobid7@outlook.de>
2021-08-09 16:01:23 +0200

    Update nightlyreader.hpp

--- source/nightlyreader.hpp
+++ source/nightlyreader.hpp
@@ -1,9 +1,15 @@
 #include "ini.hpp"
 
-struct DB =
+struct DB_Entry = 
 {
      std::string name;
-     std::string dl_list;
+     std::string dl_link;
+}
+struct DB =
+{
+     std::string reppname;
+     std::string repo_host;
+     std::vector<DB_Entry> e_list;
 };
 
 struct APPH =
