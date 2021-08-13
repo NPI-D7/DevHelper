0e79cba606755424733f07df91c8a39ddfe21162
Tobi <tobid7@outlook.de>
2021-08-13 21:18:57 +0200

    Update nightlyreader.cpp

--- source/nightlyreader.cpp
+++ source/nightlyreader.cpp
@@ -5,3 +5,19 @@ void DBLoader::DownloadEntry(int index)
      if (index <= (int)this->db.e_list.size() + 1) downloadToFile(this->db.e_list[index].dl_link, "sdmc:/DevHelper/dbs/" + GetFilename(this->db.e_list[index].dl_link));
      else int i = 0;
 }
+
+void DBLoader::DoenloadDB(std::string link)
+{
+     for (auto const& it : ini)
+     {
+	     auto const& section = it.first;
+	     auto const& collection = it.second;
+	     std::cout << "[" << section << "]" << std::endl;
+	     for (auto const& it2 : collection)
+	     {
+		     auto const& key = it2.first;
+		     auto const& value = it2.second;
+		     std::cout << key << "=" << value << std::endl;
+	     }
+     }
+}
