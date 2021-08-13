d19bac5343af57b46cac6b8bdb2fa41eafafbdf8
Tobi <tobid7@outlook.de>
2021-08-13 21:42:32 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -11,12 +11,7 @@ int main()
         
         RenderD7::OnScreen(Top);
         RenderD7::DrawText(0, 0, 0.7f, C2D_Color32(255, 255, 255, 255), RenderD7::GetFramerate());
-        for (int i = 0; i < 255; i++)
-        {
-             RenderD7::DrawRect(i, 55, i, 20, C2D_Color32((u8)i, 0, 0, 255));
-             RenderD7::DrawRect(i, 85, i, 20, C2D_Color32(0, (u8)i, 0, 255));
-             RenderD7::DrawRect(i, 115, i, 20, C2D_Color32(0, 0, (u8)i, 255));
-        }
+        
         C3D_FrameEnd(0);
     }
     RenderD7::Exit::Main();
