6882e3aa2cf8e40a2144463721e3deafae0af72a
Tobi <tobid7@outlook.de>
2021-08-14 07:55:27 +0200

    Update scenes.hpp

--- source/scenes.hpp
+++ source/scenes.hpp
@@ -1,4 +1,5 @@
 #include "renderd7.hpp"
+#include "nightlyreader.hpp"
 
 class DBSel : public RenderD7::Scene
 {
@@ -7,5 +8,6 @@ class DBSel : public RenderD7::Scene
      void Draw(void) const override;
      void Logic(u32 hDown, u32 hHeld, u32 hUp, touchPosition touch) override;
      private:
+     DBLoader dbld;
      int dirsel = 0;
 };
