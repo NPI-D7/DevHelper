eb4afe5dec3f6801d7246a9a8484cbcd23950c51
Tobi <tobid7@outlook.de>
2021-08-14 08:10:33 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,16 @@
 #include "renderd7.hpp"
 #include "nightlyreader.hpp"
+#include "scenes.hpp"
 
 int main()
 {
     RenderD7::Init::Main();
-  
+    RenderD7::Scene::Load(std::make_unique<DBSel>());
     while(RenderD7::MainLoop())
     {
         if (d7_hDown & KEY_START) RenderD7::ExitApp();
-        RenderD7::Scene::DoDraw();
-        RenderD7::Scene::DoLogic(d7_hDown, d7_hHeld, d7_hUp, d7_touch);
+        RenderD7::Scene::doDraw();
+        RenderD7::Scene::doLogic(d7_hDown, d7_hHeld, d7_hUp, d7_touch);
         C3D_FrameEnd(0);
     }
     RenderD7::Exit::Main();
