bcec18bfc2398ba95b6651b95aef61c42b155b2c
Tobi <tobid7@outlook.de>
2021-08-14 09:35:29 +0200

    Update scenes.cpp

--- source/scenes.cpp
+++ source/scenes.cpp
@@ -43,6 +43,7 @@ void DBSel::Draw(void) const
 
 void DBSel::Logic(u32 hDown, u32 hHeld, u32 hUp, touchPosition touch)
 {
+    if (hDown & KEY_A) dbld.DownloadEntry(dirsel);
     if (hDown & KEY_UP && dirsel > 0) dirsel--;
     if (hDown & KEY_DOWN && dirsel < (int)dbld.db.e_list.size() - 1) dirsel++;
     if (hDown & KEY_LEFT && dirsel - 6 > 0) dirsel -= 6;
