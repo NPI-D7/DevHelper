36df3ac41ade4351fcdcccb423ecbe7c8e614997
Tobi <tobid7@outlook.de>
2021-08-14 10:17:44 +0200

    Update scenes.hpp

--- source/scenes.hpp
+++ source/scenes.hpp
@@ -10,5 +10,13 @@ class DBSel : public RenderD7::Scene
      private:
      DBLoader dbld;
      int dirsel = 0;
-
+     std::vector<RenderD7::TLBtn> = {
+          {0, 30, 400, 30},
+          {0, 60, 400, 30},
+          {0, 90, 400, 30},
+          {0, 120, 400, 30},
+          {0, 150, 400, 30},
+          {0, 180, 400, 30},
+          {0, 210, 400, 30}
+     };
 };
