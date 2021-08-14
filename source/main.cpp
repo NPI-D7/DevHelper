4477ed2f73fa1bbd572b531fb8992b0d0e12820d
Tobi <tobid7@outlook.de>
2021-08-14 08:06:25 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,22 +1,15 @@
 #include "renderd7.hpp"
 #include "nightlyreader.hpp"
 
-std::string standard = "https://github.com/NPI-D7/nightlys/raw/master/nightlys-Database.ini";
-DBLoader dbl;
 int main()
 {
     RenderD7::Init::Main();
-    dbl.LoadDB(standard);
-    dbl.DownloadEntry(4);
-
+  
     while(RenderD7::MainLoop())
     {
         if (d7_hDown & KEY_START) RenderD7::ExitApp();
-        RenderD7::OnScreen(Bottom);
-        
-        RenderD7::OnScreen(Top);
-        RenderD7::DrawText(0, 0, 0.7f, C2D_Color32(255, 255, 255, 255), RenderD7::GetFramerate() + dbl.GetRepoName() + dbl.GetRepoHost());
-        
+        RenderD7::Scene::DoDraw();
+        RenderD7::Scene::DoLogic(d7_hDown, d7_hHeld, d7_hUp, d7_touch);
         C3D_FrameEnd(0);
     }
     RenderD7::Exit::Main();
