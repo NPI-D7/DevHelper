18190ecd58b9bd0a220bb80abd09feeb0edffd88
Tobi <tobid7@outlook.de>
2021-08-19 12:52:16 +0200

    Update nightlyreader.cpp

--- source/nightlyreader.cpp
+++ source/nightlyreader.cpp
@@ -11,7 +11,9 @@ void DBLoader::DownloadEntry(int index)
      mkdir(s.c_str(), 0777);
      if (index <= (int)this->db.e_list.size() + 1) downloadToFile(this->db.e_list[index].dl_link, "sdmc:/DevHelper/dbs/" + DBLoader::GetRepoName() + "/" + GetFileName<std::string>(this->db.e_list[index].dl_link));
      else {/** Do Nothing Yet*/}
+     D_P();
      this->LoadEntry(GetFileName<std::string>(this->db.e_list[index].dl_link));
+     D_P();
 }
 
 void DBLoader::LoadDB(std::string link)
@@ -50,17 +52,23 @@ void DBLoader::LoadDB(std::string link)
 void DBLoader::LoadEntry(std::string filename)
 {
      INI::INIFile file("sdmc:/DevHelper/dbs/" + filename);
+     D_P();
      INI::INIStructure ini;
+     D_P();
      file.read(ini);
+     D_P();
      APPH dbe;
+     D_P();
      for (auto const& it : ini)
      {
+             D_P();
 	     auto const& section = it.first;
 	     std::cout << "[" << section << "]" << std::endl;
              this->appsecs.push_back(section);
      }
-     for (int i = 0; i < (int)this->secs.size(); i++)
+     for (int i = 0; i < (int)this->appsecs.size(); i++)
      {
+          D_P();
           dbe = {ini[this->appsecs[i]]["name"], ini[this->appsecs[i]]["author"], ini[this->appsecs[i]]["commit_tag"], ini[this->appsecs[i]]["desc"], ini[this->appsecs[i]]["version"], ini[this->appsecs[i]]["3dsx"], ini[this->appsecs[i]]["cia"]};
           this->versions.push_back(dbe);
      }
