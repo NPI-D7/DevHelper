b9a52768f3ff3c273f28a402d41f360beb314750
Tobi <tobid7@outlook.de>
2021-08-19 12:41:54 +0200

    Update scenes.hpp

--- source/scenes.hpp
+++ source/scenes.hpp
@@ -10,7 +10,7 @@ class DBSel : public RenderD7::Scene
      private:
      int dirsel = 0;
      int SPos = 0;
-     DBLoader dbld;
+     //DBLoader dbld;
      std::vector<RenderD7::TLBtn> lst = {
           {0, 30, 400, 30},
           {0, 60, 400, 30},
@@ -29,7 +29,7 @@ class appver : public RenderD7::Scene
      void Draw(void) const override;
      void Logic(u32 hDown, u32 hHeld, u32 hUp, touchPosition touch) override;
      private:
-     DBLoader dbld;
+     //DBLoader dbld;
      int dirsel = 0;
      int SPos = 0;
      std::vector<RenderD7::TLBtn> lst = {
