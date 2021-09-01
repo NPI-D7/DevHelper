81b81a25ca646b7f56081c1653c831e39b055735
Tobi <tobid7@outlook.de>
2021-09-01 15:53:33 +0200

    Update scenes.hpp

--- source/scenes.hpp
+++ source/scenes.hpp
@@ -21,24 +21,3 @@ class DBSel : public RenderD7::Scene
           {0, 210, 400, 30}
      };
 };
-
-class appver : public RenderD7::Scene
-{
-     public:
-     appver();
-     void Draw(void) const override;
-     void Logic(u32 hDown, u32 hHeld, u32 hUp, touchPosition touch) override;
-     private:
-     //DBLoader dbld;
-     int dirsel = 0;
-     int SPos = 0;
-     std::vector<RenderD7::TLBtn> lst = {
-          {0, 30, 400, 30},
-          {0, 60, 400, 30},
-          {0, 90, 400, 30},
-          {0, 120, 400, 30},
-          {0, 150, 400, 30},
-          {0, 180, 400, 30},
-          {0, 210, 400, 30}
-     };
-};
