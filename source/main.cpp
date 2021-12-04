14a56db5c15f00a688150db720517c832ebeacd2
Tobi <tobid7@outlook.de>
2021-12-04 01:24:45 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -4,14 +4,13 @@
 
 int main()
 {
-    RenderD7::Init::Main();
-    consoleInit(GFX_BOTTOM, NULL);
+    RenderD7::Init::Main("Dev-Helper");
+   // consoleInit(GFX_BOTTOM, NULL);
     RenderD7::Scene::Load(std::make_unique<DBSel>());
     while(RenderD7::MainLoop())
     {
         if (d7_hDown & KEY_START) RenderD7::ExitApp();
-        RenderD7::Scene::doDraw();
-        RenderD7::Scene::doLogic(d7_hDown, d7_hHeld, d7_hUp, d7_touch);
+  
         C3D_FrameEnd(0);
     }
     RenderD7::Exit::Main();
