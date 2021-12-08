5ec637a6e18b8816e59f85dd6dbd6c3de084e624
Tobi <tobid7@outlook.de>
2021-12-08 17:37:47 +0100

    Update nightlyreader.hpp

--- source/nightlyreader.hpp
+++ source/nightlyreader.hpp
@@ -36,6 +36,7 @@ class DBLoader
      void LoadDB(std::string link);
      void DownloadEntry(int index);
      void LoadEntry(int index);
+     void DownloadApp(int index, bool _3dsx = true);
      std::string GetRepoName(){ return this->db.reponame; }
      std::string GetRepoHost(){ return this->db.repo_host; }
      DB db;
