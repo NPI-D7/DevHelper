e9e6e95378a5a5f15ca76c0d8cc2ad2a6aa468ee
Tobi <tobid7@outlook.de>
2022-03-29 13:24:05 +0200

    Fix???

--- source/main.cpp
+++ source/main.cpp
@@ -3,21 +3,21 @@
 #include "scenes.hpp"
 #include "log.hpp"
 
-Log log;
+Log flog;
 
 int main()
 {
-    log.Init("DevHelper.log");
+    flog.Init("DevHelper.log");
     RenderD7::Init::Main("Dev-Helper");
-    log.Write("Loaded RenderD7");
+    flog.Write("Loaded RenderD7");
     RenderD7::Scene::Load(std::make_unique<DBSel>());
-    log.Write("Loading Scene");
+    flog.Write("Loading Scene");
     while(RenderD7::MainLoop())
     {
         if (d7_hDown & KEY_START) RenderD7::ExitApp();
   
         C3D_FrameEnd(0);
     }
-    log.Write("Call RD7::Exit::Main");
+    flog.Write("Call RD7::Exit::Main");
     RenderD7::Exit::Main();
 }
