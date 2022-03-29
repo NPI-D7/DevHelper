d8182d97f324c59abe748b38711554c860329202
Tobi <tobid7@outlook.de>
2022-03-29 13:19:53 +0200

    Debug because app crashes

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,23 @@
 #include "renderd7.hpp"
 #include "nightlyreader.hpp"
 #include "scenes.hpp"
+#include "log.hpp"
+
+Log log;
 
 int main()
 {
+    log.Init("DevHelper.log");
     RenderD7::Init::Main("Dev-Helper");
+    log.Write("Loaded RenderD7");
     RenderD7::Scene::Load(std::make_unique<DBSel>());
+    log.Write("Loading Scene");
     while(RenderD7::MainLoop())
     {
         if (d7_hDown & KEY_START) RenderD7::ExitApp();
   
         C3D_FrameEnd(0);
     }
+    log.Write("Call RD7::Exit::Main");
     RenderD7::Exit::Main();
 }
